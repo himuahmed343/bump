ad01aa7f7e026ab0c95e36f07884d7c9a853b6d9
Himu Ahmed <64636115+himuahmed343@users.noreply.github.com>
2021-10-21 16:27:32 +0600

    Use React, Redux, Hook

---
+++ src/containers/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <Link to="/">
+                <h2>Bump.</h2>
+
+                </Link>
+            </div>
+            
+        </div>
+    )
+}
